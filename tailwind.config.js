22a53f89ed1a4d955d17b61f6516e8964b81e980
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2022-08-17 04:00:31 +0900

    Use tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{ts,tsx}", "./public/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
