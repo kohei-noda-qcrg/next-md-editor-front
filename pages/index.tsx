e07e1ae29315cbaf9dd39abaf172779500636a94
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2022-08-17 05:32:24 +0900

    Fetch markdown from /posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,26 @@ import "@uiw/react-markdown-preview/markdown.css";
 import type { NextPage } from "next";
 import { useState } from "react";
 import rehypeSanitize from "rehype-sanitize";
+import fs from "fs";
+import path from "path";
+
+type Props = {
+  posts: string[];
+};
 const MarkdownEditor = dynamic(() => import("@uiw/react-markdown-editor"), {
   ssr: false,
 });
-const Home: NextPage = () => {
+const Home: NextPage<Props> = (props) => {
   const [value, setValue] = useState(
-    `**Hello world!!!** <IFRAME SRC=\"javascript:javascript:window.alert('hello');;\"></IFRAME><script>window.alert('hello');</script>`
+    props.posts[0] ? props.posts[0] : "## Hello World"
   );
   const handleChange = (value: string) => {
     setValue(value);
   };
+  const handleSave = () => {
+    console.log(value);
+  };
+  console.log(props.posts);
   return (
     <div className="h-screen">
       <MarkdownEditor
@@ -23,8 +33,28 @@ const Home: NextPage = () => {
         onChange={(value, _) => handleChange(value)}
         previewProps={{ rehypePlugins: [[rehypeSanitize]] }}
       />
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handleSave}
+      >
+        Save
+      </button>
     </div>
   );
 };
 
 export default Home;
+
+export async function getStaticProps() {
+  const res = fs.readdirSync(path.join("posts"));
+
+  const posts = res.map((file) => {
+    const slug = file.replace(/\.md$/, "");
+    const md = fs.readFileSync(path.join("posts", file), "utf8");
+    console.log(md);
+    return md;
+  });
+  return {
+    props: { posts },
+  };
+}
---
+++ posts/test.md
@@ -0,0 +1,5 @@
+# Test
+
+**Hello world!!!**
+<IFRAME SRC=\"javascript:javascript:window.alert('hello');;\"></IFRAME>
+<script>window.alert('hello');</script>
