130ada3a4e1edac466710ce210ae37ce85120593
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2022-08-17 09:34:04 +0900

    Add [post].tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,71 +1,44 @@
 import dynamic from "next/dynamic";
 import "@uiw/react-markdown-editor/markdown-editor.css";
 import "@uiw/react-markdown-preview/markdown.css";
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import { useState } from "react";
 import rehypeSanitize from "rehype-sanitize";
 import fs from "fs";
 import path from "path";
+import Post from "./post/[post]";
+import Link from "next/link";
 
 type Props = {
   posts: string[];
 };
-const MarkdownEditor = dynamic(() => import("@uiw/react-markdown-editor"), {
-  ssr: false,
-});
-const url = "http://localhost:3000/api/post/testmd";
+
 const Home: NextPage<Props> = (props) => {
-  const [value, setValue] = useState(
-    props.posts[0] ? props.posts[0] : "## Hello World"
-  );
-  const handleChange = (value: string) => {
-    setValue(value);
-  };
-  const handleSave = () => {
-    fetch(url, {
-      method: "POST",
-      body: value,
-    })
-      .then((res) => {
-        // alert(res.statusText);
-        alert("Saved");
-        console.log(res);
-      })
-      .catch((err) => {
-        alert("Could not saved.");
-        console.log(err);
-      });
-  };
-  return (
-    <div className="h-screen">
-      <MarkdownEditor
-        className="h-4/5"
-        visible={true}
-        value={value}
-        onChange={(value, _) => handleChange(value)}
-        previewProps={{ rehypePlugins: [[rehypeSanitize]] }}
-      />
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={handleSave}
+  return props.posts.map((post) => {
+    return (
+      <div
+        key={post}
+        className="flex flex-col items-center justify-center h-24"
       >
-        Save
-      </button>
-    </div>
-  );
+        <Link href={`/post/${post}`}>
+          <a>{post}</a>
+        </Link>
+      </div>
+    );
+  });
 };
 
 export default Home;
 
-export async function getStaticProps() {
-  const res = fs.readdirSync(path.join("posts"));
-
-  const posts = res.map((file) => {
-    const slug = file.replace(/\.md$/, "");
-    const md = fs.readFileSync(path.join("posts", file), "utf8");
-    return md;
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const res = fs.readdirSync(path.join("posts"), "utf-8");
+  const posts = res.map((post) => {
+    const slug = post.replace(/\.md$/, "");
+    console.log(slug);
+    return slug;
   });
+  console.log("posts", posts);
   return {
     props: { posts },
   };
-}
+};
---
+++ pages/post/[post].tsx
@@ -0,0 +1,71 @@
+import dynamic from "next/dynamic";
+import "@uiw/react-markdown-editor/markdown-editor.css";
+import "@uiw/react-markdown-preview/markdown.css";
+import type { GetServerSideProps, NextPage } from "next";
+import { useState } from "react";
+import rehypeSanitize from "rehype-sanitize";
+import fs from "fs";
+import path from "path";
+import { useRouter } from "next/router";
+
+type Props = {
+  post: string;
+};
+const MarkdownEditor = dynamic(() => import("@uiw/react-markdown-editor"), {
+  ssr: false,
+});
+const url = "http://localhost:3000/api/post/testmd";
+const Post: NextPage<Props> = (props) => {
+  const router = useRouter();
+  const [value, setValue] = useState(
+    props.post ? props.post : "## Hello World"
+  );
+  const handleChange = (value: string) => {
+    setValue(value);
+  };
+  const handleSave = () => {
+    fetch(url, {
+      method: "POST",
+      body: value,
+    })
+      .then((res) => {
+        // alert(res.statusText);
+        alert("Saved");
+        console.log(res);
+      })
+      .catch((err) => {
+        alert("Could not saved.");
+        console.log(err);
+      });
+  };
+  return (
+    <div className="h-screen">
+      <MarkdownEditor
+        className="h-4/5"
+        visible={true}
+        value={value}
+        onChange={(value, _) => handleChange(value)}
+        previewProps={{ rehypePlugins: [[rehypeSanitize]] }}
+      />
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handleSave}
+      >
+        Save
+      </button>
+    </div>
+  );
+};
+
+export default Post;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const res = fs.readFileSync(path.join("posts", context.params?.post + ".md"), "utf-8").toString();
+    const props: Props = {
+        post: res,
+    };
+
+    return {
+        props,
+    };
+}
