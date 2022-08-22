3b2eb41c854b7beaa4fb8d3bafbc79628f53f8bf
Kohei Noda <koehi-noda@hiroshima-u.ac.jp>
2022-08-22 16:43:51 +0900

    Fix JSX element error (ref: https://qiita.com/motchi0214/items/c0aaa360b5f141d93a42)

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -1,23 +1,41 @@
-import type { GetServerSideProps, NextPage } from "next";
 import Link from "next/link";
+import type { GetServerSideProps } from "next";
+import fs from "fs";
+import path from "path";
+
 
 type Props = {
   posts: string[];
 };
 
 const Posts = (props: Props) => {
-  return props.posts.map((post) => {
-    return (
-      <div
-        key={post}
-        className="flex flex-col items-center justify-center h-24"
-      >
-        <Link href={`/post/${post}`}>
-          <a>{post}</a>
-        </Link>
-      </div>
-    );
-  });
+  return <>
+      {props.posts.map((post) => {
+      return (
+        <div
+          key={post}
+          className="flex flex-col justify-center h-24 ml-3"
+        >
+          <Link href={`/post/${post}`}>
+            <a>{post}</a>
+          </Link>
+        </div>
+      );
+    })}
+  </>
 };
 
 export default Posts;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const res = fs.readdirSync(path.join("posts"), "utf-8");
+  const posts = res.map((post) => {
+    const slug = post.replace(/\.md$/, "");
+    console.log(slug);
+    return slug;
+  });
+  console.log("posts", posts);
+  return {
+    props: { posts },
+  };
+};
