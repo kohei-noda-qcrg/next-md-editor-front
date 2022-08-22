09fe535a77ac4f784ef88607518bb4c027259198
Kohei Noda <koehi-noda@hiroshima-u.ac.jp>
2022-08-22 16:45:07 +0900

    Add Form handleSubmit

--- pages/modal.tsx
+++ pages/modal.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Link from "next/link";
-import { useCallback, useRef, useState } from "react";
+import { useRouter } from "next/router";
+import { FormEvent, useCallback, useRef, useState } from "react";
 import ReactModal from "react-modal";
 
 const Modal: NextPage = () => {
@@ -14,6 +15,7 @@ const Modal: NextPage = () => {
       transform: "translate(-50%, -50%)",
     },
   };
+  const router = useRouter();
   const inputRef = useRef<HTMLInputElement>(null);
   const [fileName, setFileName] = useState("");
   const [isOpen, setIsOpen] = useState(false);
@@ -31,6 +33,12 @@ const Modal: NextPage = () => {
   const handleFileName = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFileName(e.target.value);
   };
+  const handleSubmit = (e: FormEvent<HTMLFormElement> | undefined) =>{
+    if(e){
+      e.preventDefault()
+      router.push(`/post/${fileName}`)
+    }
+  }
   return (
     <>
       <div className="p-2">
@@ -48,14 +56,16 @@ const Modal: NextPage = () => {
           onAfterOpen={handleFocusInput}
           style={customStyles}
         >
-          <input
-            ref={inputRef}
-            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-            type="text"
-            placeholder="Enter filename"
-            value={fileName}
-            onChange={handleFileName}
-          />
+          <form onSubmit={handleSubmit}>
+            <input
+              ref={inputRef}
+              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+              type="text"
+              placeholder="Enter filename"
+              value={fileName}
+              onChange={handleFileName}
+            />
+          </form>
           <div className="pt-2 space-x-2 my-2">
             <button
               className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
@@ -65,7 +75,7 @@ const Modal: NextPage = () => {
             </button>
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
               <Link href={`/post/${fileName}`}>
-                <a>Create This File</a>
+                <a>Create a new Markdown file</a>
               </Link>
             </button>
           </div>
